17b930e8cfe51740a0b3ea1d40e49b94490afb73
Nnaemekac <nnaemekachimezie5@gmail.com>
2024-04-10 20:36:31 -0700

    ---

--- src/components/EventTracking.jsx
+++ src/components/EventTracking.jsx
@@ -9,7 +9,7 @@ export default function EventTracking(){
             <section className="xui-pt-2 xui-pb-5 xui-container">
                 <div className='xui-row xui-flex-ai-center'>
                     <div className='xui-lg-col-6 xui-col-12'>
-                        <h3 className="xui-font-sz-180 xui-text-center bold-font features-underline xui-pos-relative xui-d-inline-block">Lifecycle Event Tracking</h3>
+                        <h3 className="xui-font-sz-180 bold-font features-underline xui-pos-relative xui-d-inline-block">Lifecycle Event Tracking</h3>
                         <p className="xui-font-sz-80 iyd-text-blue xui-opacity-8 xui-line-height-1-half">At ItsYourDay, we understand that life is filled with special moments worth celebrating. That's why we've introduced a unique feature that allows users to track and plan every milestone event in their lives, from the birth of a child to their graduation and beyond.</p>
                         <HashLink style={{"border": "1px solid #020031"}} className="iyd-text-blue xui-mt-1 xui-bdr-rad-10 xui-text-dc-none xui-d-inline-flex xui-flex-ai-center xui-py-half xui-pl-1 xui-pr-half">
                             <p className="xui-font-sz-80">Get started</p>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,16 +12,16 @@ export default function Header(){
                         <Navbar />
                         <div className="xui-d-flex xui-flex-ai-center xui-lg-flex-dir-row xui-flex-dir-column-reverse xui-container">
                             <div className="xui-lg-col-7 xui-col-12">
-                                <h1 className="xui-font-sz-220 xui-lg-w-fluid-80 xui-w-fluid-100 bold-font xui-font-w-100">The Future of Event Management System just got better with <span className="xui-font-w-700 xui-pos-relative header-underline iyd-text-blue">ItsYourDay</span></h1>
+                                <h1 className="xui-lg-font-sz-220 xui-font-sz-180 xui-lg-w-fluid-80 xui-w-fluid-100 bold-font xui-font-w-100">The Future of Event Management System just got better with <span className="xui-font-w-700 xui-pos-relative header-underline iyd-text-blue">ItsYourDay</span></h1>
                                 <div className="xui-d-flex xui-flex-ai-center xui-grid-gap-1 xui-mt-1-half">
                                     <HashLink className="iyd-blue-bg xui-text-white xui-bdr-rad-10 xui-text-dc-none xui-d-inline-flex xui-flex-ai-center xui-py-half xui-pl-1 xui-pr-half">
-                                        <p className="xui-font-sz-90">Learn more</p>
+                                        <p className="xui-lg-font-sz-90 xui-font-sz-70">Learn more</p>
                                         <span className="xui-w-35 xui-h-35 xui-ml-1 xui-bg-white xui-bdr-rad-circle xui-d-flex xui-flex-jc-center xui-flex-ai-center">
                                             <ArrowRight color="#020031" />
                                         </span>
                                     </HashLink>
                                     <HashLink style={{"border": "1px solid #020031"}} className="iyd-text-blue xui-bdr-rad-10 xui-text-dc-none xui-d-inline-flex xui-flex-ai-center xui-py-half xui-pl-1 xui-pr-half">
-                                        <p className="xui-font-sz-90">Get started</p>
+                                        <p className="xui-lg-font-sz-90 xui-font-sz-70">Get started</p>
                                         <span className="xui-w-35 xui-h-35 xui-ml-1 iyd-blue-bg xui-bdr-rad-circle xui-d-flex xui-flex-jc-center xui-flex-ai-center">
                                             <ArrowRight color="#FFF" />
                                         </span>
